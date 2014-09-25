eaac847f54a752ad4b6ae2a44fe4ab6693687a99
Peter <Peter@192.168.1.2>
2014-09-25 08:02:45 +0100

    a

--- mapApp.js
+++ mapApp.js
@@ -4,10 +4,25 @@ app.controller("places", function($scope) {
 	$scope.places = [];
 	$scope.newPlace = {};
 
+	$scope.PLACE_TYPES = [{id: 'party', label: 'Bulika'},
+						{id : 'food',  label: 'Kajahely'},
+						{id : 'apartment', label: 'Kiado lakas'},
+						];
+
 	$scope.placeSet = function(newPlace) {
-		newPlace.marker = selectedMarker;
 		$scope.places.push(newPlace);
-		setMarker(selectedMarker);
+
+	    var infoContent = "<h2>" + newPlace.name + "</h2>"
+	    				+ "<h3>" + newPlace.type + "</h3>"
+	    				+ newPlace.description;
+
+	    var infowindow = new google.maps.InfoWindow({
+	        content: infoContent,
+	    });
+	    google.maps.event.addListener(newPlace.marker, "click", function() {
+	     	infowindow.open(map, newPlace.marker);
+	    });
+
 		$scope.newPlace = {};
 		console.log($scope.places);
 	};
@@ -15,11 +30,28 @@ app.controller("places", function($scope) {
 	$scope.setNewPlace = function(lng, lat) {
 		$scope.newPlace.lng = lng;
 		$scope.newPlace.lat = lat;
+    	var marker = new google.maps.Marker({
+      		position: new google.maps.LatLng(lng, lat),
+      		animation: google.maps.Animation.DROP,
+      		//icon: markerIcon,
+      		draggable : true,
+    	});
+    	marker.setMap(map);
+    	map.setCenter(marker.getPosition());
+    	$scope.newPlace.marker = marker;
+    	$scope.newPlace.show = true;
 		$scope.$apply();
 	};
 
-	$scope.newPlaceAddressLookup = function() {
-
+	$scope.newPlaceAddressLookup = function(place) {
+		geocoder.geocode({address : place.address}, function(result, status) {
+			if (status == google.maps.GeocoderStatus.OK) {
+				console.log(result[0].geometry.location);
+				$scope.setNewPlace(result[0].geometry.location.k, result[0].geometry.location.B);
+			} else {
+				console.log('ERROR: ' + status);
+			}
+		});
 	};
 
 	$scope.showPlace = function(place) {
